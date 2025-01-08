d58847029766e8fd0b7353edb469c191fe9e9ad5
rodolfosantiago20 <rodolfosant.braga@gmail.com>
2025-01-08 01:12:04 -0300

    Projeto cafeteria na branch develop

---
+++ Projeto-cafeteria/scripts.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector(".modal")
+const mascara = document.querySelector(".mascara-modal")
+
+function clicounoModal () {
+    modal.style.left = '50%'
+    mascara.style.visibility = 'visible'
+}
+
+function esconderModal () {
+    modal.style.left = '-30%'
+    mascara.style.visibility = 'hidden'
+}
